4124405bf97bac249b830e13b8944920ec44036c
Katie Strong <katiestrong123@gmail.com>
2021-05-26 16:51:55 +0100

    layout component added

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+function about(props) {
+  return (
+    <div>
+      <head>
+        <title>About webdev</title>
+      </head>
+      <h1>About page</h1>
+    </div>
+  );
+}
+
+export default about;
